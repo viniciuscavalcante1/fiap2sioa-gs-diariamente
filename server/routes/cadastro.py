1cc50896f0265ff613eb9d6f086a852a313464ea
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-11-22 07:36:04 -0300

    adiciona tela de cadasttro

---
+++ server/routes/cadastro.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter, Request, status, Form, Query, Response, HTTPException
+from starlette.templating import Jinja2Templates
+
+router = APIRouter()
+templates = Jinja2Templates(directory='templates')
+
+@router.get("/")
+def index(request: Request):
+    context = {'request': request}
+    return templates.TemplateResponse('login.html', context)
