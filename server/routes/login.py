0b807aa830f6a209730b51014c023c09cbaf171c
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-11-22 22:55:53 -0300

    models -> login: cria lógica de autenticação baseada em variáveis globais

--- server/routes/login.py
+++ server/routes/login.py
@@ -1,12 +1,26 @@
 import hashlib
 
-from fastapi import APIRouter, Request, status, Form, Query, Response, HTTPException
+from fastapi import APIRouter, Request, status, Form, Query, Response, HTTPException, Header
 from starlette.templating import Jinja2Templates
 
 from server.routes.cadastro import arvore_usuarios
 
 router = APIRouter()
 templates = Jinja2Templates(directory='templates')
+usuario_autenticado = False
+dados_usuario = None
+
+
+def autenticar_usuario(email, senha):
+    usuario_encontrado = arvore_usuarios.buscar_por_email(email)
+    if usuario_encontrado is not None:
+        senha_hash = hashlib.md5(senha.encode()).hexdigest()
+        if senha_hash == usuario_encontrado.senha:
+            return True, {"email": email}
+        else:
+            return False, "SENHA INCORRETA"
+    else:
+        return False, "USUARIO NAO ENCONTRADO"
 
 
 @router.get("/")
@@ -19,12 +33,14 @@ def index(request: Request):
 def login(request: Request,
           email: str = Form(...),
           senha: str = Form(...)):
-    usuario_encontrado = arvore_usuarios.buscar_por_email(email)
-    if usuario_encontrado is not None:
-        senha_hash = hashlib.md5(senha.encode()).hexdigest()
-        if senha_hash == usuario_encontrado.senha:
-            return "SENHA E USUARIO CORRETO"
-        else:
-            return "SENHA INCORRETA"
+    global usuario_autenticado
+    global dados_usuario
+    usuario_autenticado, dados_usuario = autenticar_usuario(email, senha)
+    if usuario_autenticado:
+        return {"mensagem": "Usuário autenticado", "usuario_autenticado": dados_usuario}
     else:
-        return "USUARIO NAO ENCONTRADO"
+        raise HTTPException(
+            status_code=401,
+            detail="Credenciais inválidas",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
