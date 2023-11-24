80f89c2fd00a69f9d2f7133b6676a3291f042bc6
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-11-24 08:00:31 -0300

    templates -> entradas: add botão nova entrada

--- static/js/entradas.js
+++ static/js/entradas.js
@@ -1,33 +1,37 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const cards = document.querySelectorAll('.card');
+document.addEventListener('DOMContentLoaded', function () {
+    const cards = document.querySelectorAll('.card');
 
-  cards.forEach(card => {
-    card.addEventListener('click', function() {
-      this.querySelector('.card-content').classList.toggle('show');
+    cards.forEach(card => {
+        card.addEventListener('click', function () {
+            this.querySelector('.card-content').classList.toggle('show');
+        });
     });
-  });
 });
 
-    function copiar(titulo, humor, conteudo, momentoFeliz, timestamp) {
-        var copyText = `
+function copiar(titulo, humor, conteudo, momentoFeliz, timestamp) {
+    var copyText = `
 Título: ${titulo}
 Humor: ${humor}
 Conteúdo: ${conteudo}
 Momento Feliz: ${momentoFeliz}
 Timestamp: ${timestamp}`;
 
-        var textarea = document.createElement('textarea');
-        textarea.value = copyText;
-        document.body.appendChild(textarea);
+    var textarea = document.createElement('textarea');
+    textarea.value = copyText;
+    document.body.appendChild(textarea);
 
-        textarea.select();
-        document.execCommand('copy');
-        document.body.removeChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
 
-        var dados = document.getElementById('dadosCopiados');
-        dados.style.display = 'block';
+    var dados = document.getElementById('dadosCopiados');
+    dados.style.display = 'block';
 
-        setTimeout(function () {
-            dados.style.display = 'none';
-        }, 2000);
-    }
+    setTimeout(function () {
+        dados.style.display = 'none';
+    }, 2000);
+}
+
+function redirecionarParaDiario(email) {
+    window.location.href = "/diario?email=" + email;
+}
