e7b43f41d13785e251ba9059915a1806c39a3c21
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-11-23 22:33:24 -0300

    routes -> diario -> entradas: adiciona rota de entradas com json

--- server/routes/diario.py
+++ server/routes/diario.py
@@ -2,7 +2,8 @@ import json
 from datetime import datetime
 
 from fastapi import APIRouter, Request, status, Form, Query, Response, HTTPException
-from starlette.responses import RedirectResponse
+from fastapi.encoders import jsonable_encoder
+from starlette.responses import RedirectResponse, JSONResponse
 from starlette.templating import Jinja2Templates
 
 router = APIRouter()
@@ -60,3 +61,32 @@ def diario_post(request: Request,
         json.dump(dados, arquivo, indent=2)
 
     return RedirectResponse(url=f"/diario?email={email}", status_code=303)
+
+
+@router.get("/entradas")
+def index(request: Request,
+          email=Query(...)):
+    from server.routes.login import usuario_autenticado
+    from server.routes.login import email_usuario
+    if usuario_autenticado:
+        if email_usuario == email:
+            context = {'request': request}
+            with open("C:\\Users\\vinicius.cavalcante\\Documents\\GitHub\\fiap\\fiap2sioa-gs-diariamente\\data"
+                      "\\entradas_diario.json", "r") as arquivo:
+                entradas = json.load(arquivo)
+                entradas_usuario = entradas.get(email, [])
+                if not entradas_usuario:
+                    raise HTTPException(status_code=404, detail="Diário não encontrado")
+                return JSONResponse(content=jsonable_encoder(entradas_usuario))
+        else:
+            # TODO: trocar esse HTTPException por uma página bonitinha de erro
+            raise HTTPException(
+                status_code=403,
+                detail="Sem permissão",
+                headers={"WWW-Authenticate": "Bearer"}
+            )
+    else:
+        response = RedirectResponse(
+            url=f"/login",
+            status_code=status.HTTP_302_FOUND)
+        return response
