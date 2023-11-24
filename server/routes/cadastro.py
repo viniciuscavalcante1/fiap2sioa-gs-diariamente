9a9298df7d49a6af709838e5032ab59f3f417366
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-11-24 20:02:31 -0300

    data structures -> cadastro: adiciona teste com 100k usuarios

--- server/routes/cadastro.py
+++ server/routes/cadastro.py
@@ -5,6 +5,7 @@ from starlette.templating import Jinja2Templates
 from models.Usuario import Usuario
 import hashlib
 from utils.ArvoreAVL import ArvoreAVL
+import time
 
 router = APIRouter()
 templates = Jinja2Templates(directory='templates')
@@ -29,10 +30,12 @@ def json_usuarios_para_arvore_avl(json_usuarios, arvore_usuarios):
         arvore_usuarios.inserir_dados(usuario)
     return arvore_usuarios
 
+
 JSON_USUARIOS_PATH = "data/usuarios.json"
 arvore_usuarios = ArvoreAVL()
 json_usuarios = carregar_dados_json(JSON_USUARIOS_PATH)
 arvore_usuarios = json_usuarios_para_arvore_avl(json_usuarios, arvore_usuarios)
+
 
 @router.get("/")
 def index(request: Request):
@@ -55,3 +58,22 @@ def cadastro(request: Request,
         url=f"/login",
         status_code=status.HTTP_302_FOUND)
     return response
+
+
+def inserir_n_usuarios(n):
+    inicio_tempo = time.time()
+    usuarios_temp = []
+    for i in range(n):
+        email = str(i) + "@gmail.com"
+        usuario = Usuario(nome=str(i), email=email, senha=str(i))
+        usuarios_temp.append((email, usuario.dict()))
+        arvore_usuarios.inserir_dados(usuario)
+    fim_tempo = time.time()
+    json_usuarios.update(usuarios_temp)
+    salvar_dados_json(JSON_USUARIOS_PATH, json_usuarios)
+    return f"O tempo de inserção total para {n} usuários foi de {fim_tempo - inicio_tempo} segundos. Em média, {(fim_tempo - inicio_tempo)/n} segundos por usuário."
+
+
+print(inserir_n_usuarios(1000))
+print(inserir_n_usuarios(10000))
+print(inserir_n_usuarios(100000))
