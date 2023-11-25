50176f171071961afb98adb46626685701433c6e
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-11-24 22:46:33 -0300

    add roteiro video

--- server/routes/diario.py
+++ server/routes/diario.py
@@ -31,7 +31,6 @@ def index(request: Request,
             context = {'request': request, 'email': email}
             return templates.TemplateResponse('diario.html', context, status_code=200)
         else:
-            # TODO: trocar esse HTTPException por uma página bonitinha de erro
             raise HTTPException(
                 status_code=403,
                 detail="Sem permissão",
