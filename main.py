1966998e08591153c4bd74f4194412ee2bce97e2
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-11-24 21:09:04 -0300

    settings: add requirements.txt

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
+"""
+Módulo para execução do FastAPI.
+
+Para iniciar o aplicativo, execute este módulo diretamente. Exemplo:
+    ```
+    uvicorn server.app:app --host 0.0.0.0 --port 8000 --reload
+    ```
+
+"""
 import uvicorn
-from fastapi import FastAPI
-from fastapi import APIRouter, Request, status, Form, Query, Response, HTTPException
-from fastapi.responses import RedirectResponse, HTMLResponse, StreamingResponse
-from datetime import datetime, date, time, timedelta
-from pydantic import EmailStr
-from typing import Optional, List, Union, Annotated
-from fastapi.templating import Jinja2Templates
-from zoneinfo import ZoneInfo
-import io
-import json
 
 if __name__ == "__main__":
+    # Inicia o servidor Uvicorn
     uvicorn.run("server.app:app", host="0.0.0.0", port=8000, reload=True)
